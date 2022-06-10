57aa5c4fbcd375ef74d0132bb23e675cccae8c45
Daniel Andrlik <daniel@andrlik.org>
2022-06-10 11:38:29 -0400

    Add tests for unauthorized user trying to reach detail views for another user.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from audio_asset_manager.models import Artist, AssetSource, AudioAsset, Collection
+
 pytestmark = pytest.mark.django_db(transaction=True)
 
 
@@ -82,3 +84,75 @@ def test_detail_views_require_login(
         )
     assert response.status_code == 302
     assert "/accounts/login/" in response["Location"]
+
+
+@pytest.mark.parametrize("view_name", ["asset-detail", "asset-update", "asset-delete"])
+def test_wrong_user_not_authorized_all_asset_views(
+    client, django_assert_max_num_queries, user2, user_created_assets, view_name
+):
+    asset = user_created_assets[0]
+    url = reverse(f"audio_asset_manager:{view_name}", kwargs={"pk": asset.id})
+    client.force_login(user2)
+    with django_assert_max_num_queries(50):
+        response = client.get(url)
+    assert response.status_code == 403
+    with django_assert_max_num_queries(50):
+        response = client.post(url, data={})
+    assert response.status_code == 403
+    if "delete" in view_name:
+        assert AudioAsset.objects.get(pk=asset.pk)
+
+
+@pytest.mark.parametrize(
+    "view_name", ["source-detail", "source-update", "source-delete"]
+)
+def test_wrong_user_not_authorized_all_assetsource_views(
+    client, django_assert_max_num_queries, user2, user_created_assets, view_name
+):
+    source = user_created_assets[0].source
+    url = reverse(f"audio_asset_manager:{view_name}", kwargs={"pk": source.id})
+    client.force_login(user2)
+    with django_assert_max_num_queries(50):
+        response = client.get(url)
+    assert response.status_code == 403
+    with django_assert_max_num_queries(50):
+        response = client.post(url, data={})
+    assert response.status_code == 403
+    if "delete" in view_name:
+        assert AssetSource.objects.get(pk=source.id)
+
+
+@pytest.mark.parametrize(
+    "view_name", ["artist-detail", "artist-update", "artist-delete"]
+)
+def test_wrong_user_not_authorized_all_artist_views(
+    client, django_assert_max_num_queries, user2, user_created_assets, view_name
+):
+    artist = user_created_assets[0].artist
+    url = reverse(f"audio_asset_manager:{view_name}", kwargs={"pk": artist.id})
+    client.force_login(user2)
+    with django_assert_max_num_queries(50):
+        response = client.get(url)
+    assert response.status_code == 403
+    with django_assert_max_num_queries(50):
+        response = client.post(url, data={})
+    assert response.status_code == 403
+    assert Artist.objects.get(pk=artist.id)
+
+
+@pytest.mark.parametrize(
+    "view_name", ["collection-detail", "collection-update", "collection-delete"]
+)
+def test_wrong_user_not_authorized_all_collection_views(
+    client, django_assert_max_num_queries, user2, user_created_assets, view_name
+):
+    col = user_created_assets[0].collection
+    url = reverse(f"audio_asset_manager:{view_name}", kwargs={"pk": col.id})
+    client.force_login(user2)
+    with django_assert_max_num_queries(50):
+        response = client.get(url)
+    assert response.status_code == 403
+    with django_assert_max_num_queries(50):
+        response = client.post(url, data={})
+    assert response.status_code == 403
+    assert Collection.objects.get(pk=col.id)
